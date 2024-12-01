f673ce9290b9bce55769405e0daa344c3954b917
ymotawfiq <ymtawfiq2003@gmail.com>
2024-12-01 13:45:51 +0200

    A. Petya and Strings

---
+++ A/A7_Petya_and_Strings.cpp
@@ -0,0 +1,43 @@
+
+
+/*
+problem link ----> https://codeforces.com/contest/405/problem/A
+*/
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+void readString(string& s){
+    cin>>s;
+}
+
+
+void compareStrings(string& s1, string& s2, short& ans){
+    for(int i=0; i<s1.length(); i++){
+        if(tolower(s1[i])>tolower(s2[i])){
+            ans = 1;
+            return;
+        }
+        else if(tolower(s1[i])<tolower(s2[i])){
+            ans = -1;
+            return;
+        }
+    }
+    ans = 0;
+}
+
+int main(){
+
+    short ans = 0;
+    string s1, s2;
+    readString(s1);
+    readString(s2);
+    compareStrings(s1, s2, ans);
+    cout<<ans;
+
+    return 0;
+}
