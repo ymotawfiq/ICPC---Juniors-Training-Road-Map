881e96b34839f91a9f6c6a323317dad24bb2be94
ymotawfiq <ymtawfiq2003@gmail.com>
2024-12-06 00:17:35 +0200

    A. Alyona and Numbers

---
+++ A/A_Dice_Tower.cpp
@@ -0,0 +1,21 @@
+
+
+/*
+problem link ----> https://codeforces.com/contest/225/problem/A
+*/
+
+
+#include<iostream>
+#include <set>
+using namespace std;
+
+int main(){
+    
+
+    short n, x;
+    cin>>n>>x;
+
+    
+
+    return 0;
+}
