99862a3b296fb4e94d5c91aeec962b1d53686a25
ymotawfiq <ymtawfiq2003@gmail.com>
2024-12-08 19:57:39 +0200

    A. Case of the Zeros and Ones

---
+++ A/556A. Case of the Zeros and Ones.cpp
@@ -0,0 +1,25 @@
+
+/*
+problem link --> https://codeforces.com/contest/556/problem/A
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+    unsigned int n, c0 = 0, c1 = 0;
+    cin>>n;
+    char* s = new char[n+1];
+    s[n] = '\0';
+    for(int i=0; i<n; i++){
+        cin>>s[i];
+        if(s[i] == '0')c0++;
+        else c1++;
+    }
+
+    cout<< n - (2 * min(c0, c1));
+
+    return 0;
+}
