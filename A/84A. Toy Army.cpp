bccf3a85a4a445ea1347a6f40aa6cde208ed96dc
ymotawfiq <ymtawfiq2003@gmail.com>
2024-12-11 20:54:43 +0200

    A. Toy Army

---
+++ A/84A. Toy Army.cpp
@@ -0,0 +1,17 @@
+
+/*
+problem link --> https://codeforces.com/contest/84/problem/A
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    cout<< n + n / 2;
+
+    return 0;
+}
