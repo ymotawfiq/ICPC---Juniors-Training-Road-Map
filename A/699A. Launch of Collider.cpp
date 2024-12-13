1c90e005ceeb504568107b718f7f88d2c30ab0b6
ymotawfiq <ymtawfiq2003@gmail.com>
2024-12-13 20:59:07 +0200

    A. Launch of Collider

---
+++ A/699A. Launch of Collider.cpp
@@ -0,0 +1,38 @@
+/*
+problem link ----> https://codeforces.com/contest/699/problem/A
+*/
+ 
+ 
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+ 
+int main(){
+ 
+    int n;
+    string s;
+ 
+ 
+    cin>>n;
+    cin>>s;
+    int* v = new int[n];
+ 
+    for(int i=0; i<n; i++){
+        cin>>v[i];
+    }
+ 
+    int minn = INT_MAX;
+    bool flag = false;
+    for(int i=0; i<n-1; i++){
+        if(s[i] == 'R' && s[i+1] == 'L'){
+            minn = min((v[i + 1] - v[i]) / 2, minn);
+            flag = true;
+        }
+    }
+    if(flag) cout<<minn;
+    else cout<<-1;
+    
+    delete[] v;
+    return 0;
+}
