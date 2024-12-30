2e12faf9ef73ecfbff02069f0d74ac6386ed3df6
ymotawfiq <ymtawfiq2003@gmail.com>
2024-12-30 09:10:55 +0200

    A. Little Elephant and Function

---
+++ 221A. Little Elephant and Function.cs
@@ -0,0 +1,12 @@
+
+// problem link --> https://codeforces.com/contest/221/problem/A
+
+public class S
+{
+    public static void Main(string[] args)
+    {
+        short n = Convert.ToInt16(Console.ReadLine());
+        Console.Write(n + " ");
+        for(int i= 1; i < n; i++) Console.Write(i + " ");
+    }
+}
--- 373A. Collecting Beats is Fun.cs
+++ 373A. Collecting Beats is Fun.cs
@@ -6,7 +6,6 @@ public class S
     public static void Main(string[] args)
     {
         short n = Convert.ToInt16(Console.ReadLine());
-        string[] input = new string[4];
         short[] arr = new short[10];
         bool flag = true;
         for (int i = 0; i < 4; i++)
@@ -22,14 +21,6 @@ public class S
                 }
             }
         }
-        //for(int i=1; i<10; i++)
-        //{
-        //    if (arr[i] > 2 * n)
-        //    {
-        //        Console.WriteLine("NO");
-        //        return;
-        //    }
-        //}
         if (flag) Console.WriteLine("YES");
         else Console.WriteLine("NO");
     }
